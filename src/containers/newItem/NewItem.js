f161d3a5d238ac5f772c9d341871f2892b302eba
catalin <lazcatluc@gmail.com>
2018-03-18 10:27:51 +0200

    Add elements when 'Enter' is pressed

--- src/containers/newItem/NewItem.js
+++ src/containers/newItem/NewItem.js
@@ -5,7 +5,12 @@ export default (onAddItem, newItemValue, newItemChange) => (
   <div className={styles.commentForm}>
     <div>
     <input type="text" value={newItemValue} className="form-control"
-           onChange={event => newItemChange(event.target.value)} />
+           onChange={event => newItemChange(event.target.value)}
+	   onKeyPress={event => {
+				if (event.key === 'Enter') {
+				    onAddItem(event);
+				}
+                              }}/>
     </div>
     <div>
       <input type="submit" className={styles.addButton + " btn btn-primary"}
