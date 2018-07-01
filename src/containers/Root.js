0e3a5b71d4e0fca4bad09136f39d690e286ec5df
Catalin Lazar <catalin.lazar@endava.com>
2018-07-01 07:31:50 +0300

    Remove test cache text

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,8 +3,6 @@ import ShoppingList from './shopping/ShoppingList.js'
 import VisibilityToggle from './visibility/VisibilityToggle'
 import NewItem from './newItem/NewItem'
 
-// test cache
-
 const Root = ({ currentState, onClickItem, onAddItem, onRemoveItem, newItemChange, show, setItemPrice }) => (
   <div className="well" style={{border: 0, borderRadius: 0}}>
     {ShoppingList(currentState.shoppingListItems, currentState.visibilityFilter,
